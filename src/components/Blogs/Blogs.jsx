8306415f7345bc5f79204bcf27a8139c90f481ac
alaminniloy <alamin15-11132@diu.edu.bd>
2023-11-01 23:19:34 +0600

    add blog

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -11,8 +11,8 @@ const Blogs = () => {
     },[])
 
     return (
-        <div>
-            <h1>Blogs: {blogs.ID}</h1>
+        <div className="md:w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
         </div>
     );
 };
